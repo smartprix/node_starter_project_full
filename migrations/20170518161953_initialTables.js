e076b48e1a03c63beae703695bcbc9265903beeb
Shivam <shivam.agrawal@smartprix.com>
2017-11-08 16:30:50 +0530

    json -> jsonb for pg

--- migrations/20170518161953_initialTables.js
+++ migrations/20170518161953_initialTables.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
 	return knex.schema.createTableIfNotExists('Store', (table) => {
 		table.increments('id').primary();
 		table.string('name').notNullable();
@@ -11,40 +11,40 @@ exports.up = function(knex) {
 		table.timestamp('updatedAt').nullable();
 		table.timestamp('deletedAt').nullable();
 	})
-	.createTableIfNotExists('Brand', (table) => {
-		table.increments('id').primary();
-		table.string('name').notNullable();
-		table.string('aliases').notNullable();
-		table.string('status').notNullable();
-		table.timestamp('createdAt').nullable();
-		table.timestamp('updatedAt').nullable();
-		table.timestamp('deletedAt').nullable();
-	})
-	.createTableIfNotExists('Category', (table) => {
-		table.increments('id').primary();
-		table.string('name').notNullable();
-		table.string('shortName').notNullable();
-		table.string('pluralName').notNullable();
-		table.string('aliases').notNullable();
-		table.integer('parentId').notNullable();
-		table.string('status').notNullable();
-		table.json('metadata').notNullable();
-		table.timestamp('createdAt').nullable();
-		table.timestamp('updatedAt').nullable();
-		table.timestamp('deletedAt').nullable();
+		.createTableIfNotExists('Brand', (table) => {
+			table.increments('id').primary();
+			table.string('name').notNullable();
+			table.string('aliases').notNullable();
+			table.string('status').notNullable();
+			table.timestamp('createdAt').nullable();
+			table.timestamp('updatedAt').nullable();
+			table.timestamp('deletedAt').nullable();
+		})
+		.createTableIfNotExists('Category', (table) => {
+			table.increments('id').primary();
+			table.string('name').notNullable();
+			table.string('shortName').notNullable();
+			table.string('pluralName').notNullable();
+			table.string('aliases').notNullable();
+			table.integer('parentId').notNullable();
+			table.string('status').notNullable();
+			table.jsonb('metadata').notNullable();
+			table.timestamp('createdAt').nullable();
+			table.timestamp('updatedAt').nullable();
+			table.timestamp('deletedAt').nullable();
 
-		table.unique('name');
-		table.unique('shortName');
-	})
-	.createTableIfNotExists('BrandCategoryMap', (table) => {
-		table.string('brandId').notNullable();
-		table.string('categoryId').notNullable();
-	});
+			table.unique('name');
+			table.unique('shortName');
+		})
+		.createTableIfNotExists('BrandCategoryMap', (table) => {
+			table.string('brandId').notNullable();
+			table.string('categoryId').notNullable();
+		});
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
 	return knex.schema.dropTableIfExists('Store')
-	.dropTableIfExists('Brand')
-	.dropTableIfExists('Category')
-	.dropTableIfExists('BrandCategoryMap');
+		.dropTableIfExists('Brand')
+		.dropTableIfExists('Category')
+		.dropTableIfExists('BrandCategoryMap');
 };
