ebc065aa80d18fb5f95934fb887627539387e322
Shivam <shivam.agrawal@smartprix.com>
2017-11-13 16:28:58 +0530

    updated table structures

--- migrations/20170518161953_initialTables.js
+++ migrations/20170518161953_initialTables.js
@@ -6,29 +6,29 @@ exports.up = function (knex) {
 		table.text('link').notNullable();
 		table.string('domain').notNullable();
 		table.string('status').notNullable();
-		table.integer('rating').notNullable();
+		table.integer('rating').nullable();
 		table.timestamp('createdAt').nullable();
 		table.timestamp('updatedAt').nullable();
 		table.timestamp('deletedAt').nullable();
 	})
 		.createTableIfNotExists('Brand', (table) => {
 			table.increments('id').primary();
-			table.string('name').notNullable();
-			table.string('aliases').notNullable();
-			table.string('status').notNullable();
+			table.string('name').notNullable().defaultTo('');
+			table.string('aliases').notNullable().defaultTo('');
+			table.string('status').notNullable().defaultTo('');
 			table.timestamp('createdAt').nullable();
 			table.timestamp('updatedAt').nullable();
 			table.timestamp('deletedAt').nullable();
 		})
 		.createTableIfNotExists('Category', (table) => {
 			table.increments('id').primary();
-			table.string('name').notNullable();
-			table.string('shortName').notNullable();
-			table.string('pluralName').notNullable();
-			table.string('aliases').notNullable();
-			table.integer('parentId').notNullable();
-			table.string('status').notNullable();
-			table.jsonb('metadata').notNullable();
+			table.string('name').notNullable().defaultTo('');
+			table.string('shortName').notNullable().defaultTo('');
+			table.string('pluralName').notNullable().defaultTo('');
+			table.string('aliases').notNullable().defaultTo('');
+			table.integer('parentId').notNullable().defaultTo(0);
+			table.string('status').notNullable().defaultTo('');
+			table.jsonb('metadata').notNullable().defaultTo('{}');
 			table.timestamp('createdAt').nullable();
 			table.timestamp('updatedAt').nullable();
 			table.timestamp('deletedAt').nullable();
@@ -37,8 +37,8 @@ exports.up = function (knex) {
 			table.unique('shortName');
 		})
 		.createTableIfNotExists('BrandCategoryMap', (table) => {
-			table.string('brandId').notNullable();
-			table.string('categoryId').notNullable();
+			table.integer('brandId').notNullable().defaultTo(0);
+			table.integer('categoryId').notNullable().defaultTo(0);
 		});
 };
 
