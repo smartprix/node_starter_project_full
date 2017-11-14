ac3d9fbc1d2c38d3b8b0c39640cd1b2da0667012
Shivam <shivam.agrawal@smartprix.com>
2017-11-14 19:29:11 +0530

    added user table

--- migrations/20170518161953_initialTables.js
+++ migrations/20170518161953_initialTables.js
@@ -39,6 +39,28 @@ exports.up = function (knex) {
 		.createTableIfNotExists('BrandCategoryMap', (table) => {
 			table.integer('brandId').notNullable().defaultTo(0);
 			table.integer('categoryId').notNullable().defaultTo(0);
+		})
+		.createTableIfNotExists('User', (table) => {
+			table.increments('id').primary();
+			table.string('username').notNullable().defaultTo('');
+			table.string('email').notNullable().defaultTo('');
+			table.string('phone').notNullable().defaultTo('');
+			table.string('password').notNullable().defaultTo('');
+			table.timestamp('dateOfBirth').nullable();
+			table.string('name').notNullable().defaultTo('');
+			table.enum('status', ['active', 'banned'])
+				.notNullable().defaultTo('active');
+			table.text('image').nullable();
+			table.enum('gender', ['male', 'female', 'others']).notNullable().defaultTo('male');
+			table.string('facebookId').notNullable().defaultTo('');
+			table.string('twitterId').notNullable().defaultTo('');
+			table.string('googlePlusId').notNullable().defaultTo('');
+			table.string('smartprixId').notNullable().defaultTo('');
+			table.string('country').notNullable().defaultTo('');
+			table.string('timezone').notNullable().defaultTo('');
+			table.timestamp('createdAt').nullable();
+			table.timestamp('updatedAt').nullable();
+			table.timestamp('deletedAt').nullable();
 		});
 };
 
